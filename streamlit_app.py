b4c770b8e503847a632697a5871979adacf8c950
jinxinshuang <xiaoxiaojin0323@gmail.com>
2024-09-27 00:30:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,12 +82,9 @@ unique_sub_categories_sorted=unique_sub_categories.groupby([unique_sub_categorie
 
 subcategory_sales_by_year= unique_sub_categories_sorted
 
-
 subcategory_sales_by_year["Order_Year"]=subcategory_sales_by_year['Order_Date'].dt.year
-# Convert "Order_Year" to string (so that are no extra ticks on x axis)
 subcategory_sales_by_year["Order_Year"]=subcategory_sales_by_year["Order_Year"].astype(str)
 sales_by_year= subcategory_sales_by_year.groupby(["Order_Year", "Sub_Category"], as_index=False)["Sales"].sum()
-st.dataframe(sales_by_year)
 st.line_chart(sales_by_year,x="Order_Year",
 y="Sales", color="Sub_Cateogyr")
 
