9277201f773d61a500d94cdab36d7a7ac6b4685c
jinxinshuang <xiaoxiaojin0323@gmail.com>
2024-09-27 17:37:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,8 @@ unique_sub_categories_sorted=unique_sub_categories.groupby([unique_sub_categorie
 
 # Add a header
 st.subheader("Sub_Category Sales by Year")
-monthly_sales = unique_sub_categories_sorted.groupby(pd.Grouper(key='Order_Date', freq='M')).sum()
+monthly_sales = unique_sub_categories_sorted.groupby([pd.Grouper(key='Order_Date', freq='M'), 'Sub_Category']).sum().reset_index()
+
 st.line_chart(monthly_sales, x="Order_Date",
 y="Sales", color="Sub_Category")
 
